4522b3fa86443df2e2ad207e862c13e11c7a0cba
mlinescode <mikexx1264@gmail.com>
2023-09-27 23:33:17 -0600

    Se importa el componente WeatherCard

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import styles from './App.module.css'
 import Header from './components/Header/Header'
 import PlaceInformation from './components/PlaceInformation/PlaceInformation'
+import WeatherCard from './components/WeatherCard/WeatherCard'
 
 const App: React.FC = () => {
   const [darkMode, setDarkMode] = useState(false);
@@ -28,65 +29,55 @@ const App: React.FC = () => {
         {darkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode'}
       </button>
       <PlaceInformation />
-      <div>
-        <h2 className={styles.status}>Light rain</h2>
-        <div className={styles.statusPrincipal}>
-          <img className={styles.imageIcon} src="icono" alt="" />
-          <p className={styles.temperature}>12°C</p>
-        </div>
-        <div className={styles.statusPrincipal}>
-          <h3 className={styles.feelslike}>Feels like</h3>
-          <p className={styles.feelslikeTemperature}>17°C</p>
-        </div>
-      </div>
+      <WeatherCard />
       <div className={styles.temperatureContent}>
-        <div className={styles.temperatureContainer}>
-          <h2 className={styles.temperatureSubItem}>Temperature</h2>
-          <div className={styles.temperatureItems}>
-            <p>Min.</p>
-            <p>10°C</p>
+          <div className={styles.temperatureContainer}>
+            <h2 className={styles.temperatureSubItem}>Temperature</h2>
+            <div className={styles.temperatureItems}>
+              <p>Min.</p>
+              <p>10°C</p>
+            </div>
+            <div className={styles.temperatureItems}>
+              <p>Max.</p>
+              <p>10°C</p>
+            </div>
           </div>
-          <div className={styles.temperatureItems}>
-            <p>Max.</p>
-            <p>10°C</p>
+          <div className={styles.temperatureContainer}>
+            <h2 className={styles.temperatureSubItem}>Wind</h2>
+            <div className={styles.temperatureItems}>
+              <p>Speed</p>
+              <p>1.62 m/s</p>
+            </div>
+            <div className={styles.temperatureItems}>
+              <p>Deg.</p>
+              <p>36°</p>
+            </div>
           </div>
-        </div>
-        <div className={styles.temperatureContainer}>
-          <h2 className={styles.temperatureSubItem}>Wind</h2>
-          <div className={styles.temperatureItems}>
-            <p>Speed</p>
-            <p>1.62 m/s</p>
+          <div className={styles.temperatureContainer}>
+            <h2 className={styles.temperatureSubItem}>Humidity</h2>
+            <div className={styles.temperatureItems}>
+              <h3>32%</h3>
+            </div>
           </div>
-          <div className={styles.temperatureItems}>
-            <p>Deg.</p>
-            <p>36°</p>
-          </div>
-        </div>
-        <div className={styles.temperatureContainer}>
-          <h2 className={styles.temperatureSubItem}>Humidity</h2>
-          <div className={styles.temperatureItems}>
-            <h3>32%</h3>
-          </div>
-        </div>
       </div>
       <div className={styles.recomendations}>
-        <h2>Recomendations</h2>
-        <div className={styles.recomendationsList}>
-          <p>Prueba</p>
-          <p>Prueba</p>
-          <p>Prueba</p>
-          <p>Prueba</p>
-          <p>Prueba</p>
-        </div>
+          <h2>Recomendations</h2>
+          <div className={styles.recomendationsList}>
+            <p>Prueba</p>
+            <p>Prueba</p>
+            <p>Prueba</p>
+            <p>Prueba</p>
+            <p>Prueba</p>
+          </div>
       </div>
         <div className={styles.map}>
-          <h2>Map</h2>
-          <div className={styles.mapIframe}>
-            <img src="mapa" alt="" />
-          </div>
+            <h2>Map</h2>
+            <div className={styles.mapIframe}>
+              <img src="mapa" alt="" />
+            </div>
         </div>
         <div className={styles.saveButton}>
-          <button className={styles.save}>Save</button>
+            <button className={styles.save}>Save</button>
         </div>
     </div>
   )
